5a9392a43840f56c8df9b700927e6b374938e54b
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-11-05 13:48:38 +0100

    Dodaj pliki projektów.

---
+++ kot/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace kot
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ kot/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+
+namespace kot
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            string kot1 = "https://cataas.com/cat";
+            string kot2;
+            using (var webClient = new WebClient())
+            {
+                kot2 = webClient.DownloadString(kot1);
+            }
+
+            kot.Source = kot1;
+        }
+        private void OnCounterClicked1(object sender, EventArgs e)
+        {
+            string kot10 = "https://cataas.com/cat/gif";
+            string kot20;
+            using (var webClient = new WebClient())
+            {
+                kot20 = webClient.DownloadString(kot10);
+            }
+
+            kot11.Source = kot10;
+        }
+    }
+
+}
