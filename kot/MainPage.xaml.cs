2ac1e2dfacd51f42c36891776269113367c6eb86
LNenjoyer <ut_3779@zsp3zamosc.pl>
2024-11-06 09:23:55 +0100

    jeszcze krócej

--- kot/MainPage.xaml.cs
+++ kot/MainPage.xaml.cs
@@ -12,34 +12,28 @@ namespace kot
 
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            string kot01 = "https://cataas.com/cat";
-            kot0.Source = kot01;
+            kot0.Source = "https://cataas.com/cat";
         }
         private void OnCounterClicked1(object sender, EventArgs e)
         {
-            string kot11 = "https://cataas.com/cat/gif";
-            string kot12;
-            kot1.Source = kot11;
+            kot1.Source = "https://cataas.com/cat/gif";
         }
         private void OnCounterClicked2(object sender, EventArgs e)
         {
             string kot2s = kot2say.Text;
-            string kot21 = "https://cataas.com/cat/says/"+kot2s;
-            kot2.Source = kot21;
+            kot2.Source = "https://cataas.com/cat/says/" + kot2s;
         }
         private void OnCounterClicked3(object sender, EventArgs e)
         {
             string kot3s = kot3say.Text;
-            string kot31 = "https://cataas.com/cat/gif/says/" + kot3s;
-            kot3.Source = kot31;
+            kot3.Source = "https://cataas.com/cat/gif/says/" + kot3s;
         }
             private void OnCounterClicked4(object sender, EventArgs e)
         {
             string kot4s = kot4say.Text;
             string kot4fs = kot4font.Text;
             string kot4fc = kot4color.Text;
-            string kot41 = "https://cataas.com/cat/gif/says/" + kot4s+ "?fontSize="+kot4fs+"&fontColor="+kot4fc;
-            kot4.Source = kot41;
+            kot4.Source = "https://cataas.com/cat/gif/says/" + kot4s + "?fontSize=" + kot4fs + "&fontColor=" + kot4fc;
         }
     }
 }
